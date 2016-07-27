c0539997fe48e4e7b2d4dea97a1fe80529ffc26d
jixiuf <jixiuf@qq.com>
2016-07-27 18:13:28 +0800

    update player.nickname

--- src/handler/player.py
+++ src/handler/player.py
@@ -83,12 +83,15 @@ class PlayerInfoUpdateHandler(BaseHandler):
         speaker  = int(self.get_argument('speaker',0))
         kickCard = int(self.get_argument('kickCard',0))
         lastPayTime = self.get_argument('lastPayTime',0)
+        desc = self.get_argument('desc',0)
+        nickName = self.get_argument('nickName',0)
         if uin==0:
             self.write("fail")
             return
 
         yield app.DBMgr.getMoneyDB(int(server)).update(uin,gold,gem,speaker,vipValue,kickCard,watch,car,house,boat)
         yield app.DBMgr.getMoneyDB(int(server)).updatelastPayTime(uin,lastPayTime)
+        yield app.DBMgr. getUserDB().update_attr_nickname_and_desc(uin,nickName,desc)
         time.sleep(0.03)
         app.Redis.publish(redis_notify.get_server_redis_notify_channel(conf.PLATFORM,server), redis_notify.NOTIFY_TYPE_RELOAD_MONEY%(str(uin)))
         self.write("success")
