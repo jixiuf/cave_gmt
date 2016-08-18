02e64ee92f37199dcb824b9544ef6062d6634ca2
jixiuf <jixiuf@qq.com>
2016-08-18 15:13:16 +0800

    dbtemplate suport query for all shard

--- src/db/dbtemplate/mock_db.py
+++ src/db/dbtemplate/mock_db.py
@@ -3,18 +3,18 @@ from tornado_mysql import pools
 import dbtemplate
 def mockdbtemplate():
     pool= pools.Pool(
-            dict(host="127.0.0.1", port=3306, user="th_dev", passwd="th_devpass", db="test"),
+            dict(host="127.0.0.1", port=3306, user="zjh", passwd="zjh!!@@__))", db="test"),
             max_idle_connections=1,
             max_recycle_sec=3)
     return dbtemplate.DatabaseTemplateSingle(pool)
 
 def mockdbtemplateSharding():
     pool0=pools.Pool(
-        dict(host='127.0.0.1', port=3306, user='th_dev', passwd='th_devpass', db='test'),
+        dict(host='127.0.0.1', port=3306, user='zjh', passwd='zjh!!@@__))', db='test'),
         max_idle_connections=1,
         max_recycle_sec=3)
     pool1=pools.Pool(
-        dict(host='127.0.0.1', port=3306, user='th_dev', passwd='th_devpass', db='test_1'),
+        dict(host='127.0.0.1', port=3306, user='zjh', passwd='zjh!!@@__))', db='test_1'),
         max_idle_connections=1,
         max_recycle_sec=3)
     dt0=dbtemplate.DatabaseTemplateSingle(pool0)
